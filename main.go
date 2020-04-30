e00c012504dcb313728d6638316551f7bac3931d
jeanmfdias <jeanmfdias@gmail.com>
2020-04-29 22:48:54 -0300

    added switch to the options

--- main.go
+++ main.go
@@ -14,4 +14,15 @@ func main() {
 	fmt.Println("[0] Exit")
 
 	fmt.Scan(&command)
+
+	switch command {
+	case 1:
+		fmt.Println("Monitoring...")
+	case 2:
+		fmt.Println("Show logs:")
+	case 0:
+		fmt.Println("Exit, Bye!")
+	default:
+		fmt.Println("I don't understand your command typed!")
+	}
 }
