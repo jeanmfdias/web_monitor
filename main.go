1255f342548b11ec570feb3f3cb6a0f88f45276a
jeanmfdias <jeanmfdias@gmail.com>
2020-04-29 22:58:12 -0300

    create functions to organize code

--- main.go
+++ main.go
@@ -1,28 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	var command int
+	showIntro()
+	showMenu()
 
-	version := 1.0
-
-	fmt.Println("web monitor version", version)
-
-	fmt.Println("[1] Start monitor")
-	fmt.Println("[2] Show logs")
-	fmt.Println("[0] Exit")
-
-	fmt.Scan(&command)
-
-	switch command {
+	switch readCommand() {
 	case 1:
 		fmt.Println("Monitoring...")
 	case 2:
 		fmt.Println("Show logs:")
 	case 0:
 		fmt.Println("Exit, Bye!")
+		os.Exit(0)
 	default:
 		fmt.Println("I don't understand your command typed!")
+		os.Exit(-1)
 	}
+}
+
+func showIntro() {
+	version := 1.0
+	fmt.Println("web monitor version", version)
+}
+
+func showMenu() {
+	fmt.Println("[1] Start monitor")
+	fmt.Println("[2] Show logs")
+	fmt.Println("[0] Exit")
+}
+
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+	return command
 }
