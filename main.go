3c448edc6347bbc118f00ad853cca80b24002945
jeanmfdias <jeanmfdias@gmail.com>
2020-04-29 22:35:28 -0300

    added options to the program

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	var command int
+
 	version := 1.0
 
 	fmt.Println("web monitor version", version)
+
+	fmt.Println("[1] Start monitor")
+	fmt.Println("[2] Show logs")
+	fmt.Println("[0] Exit")
+
+	fmt.Scan(&command)
 }
